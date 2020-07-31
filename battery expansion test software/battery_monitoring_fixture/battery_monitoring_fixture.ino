aab36cf8293f45875161f9821459d19345dabba4
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2020-07-31 09:46:46 -0500

    initial commit

---
+++ battery expansion test software/battery_monitoring_fixture/battery_monitoring_fixture.ino
@@ -0,0 +1,124 @@
+#include <SD.h>
+#include <SPI.h>
+enum State { CHARGING, DISCHARGING };
+State state;
+State prevState;
+
+long currentTime;
+long lastTime;
+const long period = 5 * (60 * 60 * 1000);
+
+const float dischargeThreshold = 2.9;
+
+const int chargeStatus = 0;
+const int chargeEnableLV = 1;
+const int drainEnableLV = 2;
+const int chargeIndicator = 3;
+const int drainIndicator = 4;
+
+const int batteryADC = 23;
+
+const int chipSelect = BUILTIN_SDCARD;
+const int builtInLED = 13;
+
+int batteryCounts = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(chargeStatus, INPUT);
+  pinMode(chargeEnableLV, OUTPUT);
+  pinMode(drainEnableLV, OUTPUT);
+  pinMode(chargeIndicator, OUTPUT);
+  pinMode(drainIndicator, OUTPUT);
+
+  pinMode(batteryADC, INPUT);
+
+  pinMode(builtInLED, OUTPUT);
+
+  // set max battery counts to 4095
+  // 3.3V = 4095 counts
+  // 1 count = .81mV
+  analogReadResolution(12);
+
+  if (!SD.begin(chipSelect)) {
+    digitalWrite(builtInLED, HIGH);
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  digitalWrite(builtInLED, LOW);
+  Serial.println("card initialized.");
+
+  digitalWrite(drainEnableLV, HIGH);
+  delay(100);
+  digitalWrite(chargeEnableLV, LOW);
+
+  digitalWrite(drainIndicator, LOW);
+  digitalWrite(chargeIndicator, HIGH);
+  delay(100);
+}
+
+void loop()
+{
+
+  //batteryCounts = analogRead(batteryADC);
+
+  String dataString = "";
+  dataString += String(millis());
+  dataString += ",";
+  dataString += String(analogRead(batteryADC));
+  dataString += ",";
+  dataString += String(state);
+  // open the file.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+
+  delay(5 * 60 * 1000);
+
+  prevState = state;
+
+  Serial.println(digitalRead(chargeStatus));
+
+  if ((analogRead(batteryADC) < 3600) && (state == DISCHARGING)) {
+    state = CHARGING;
+    Serial.println("charging");
+  } else if ((digitalRead(chargeStatus)) && (state == CHARGING)) {
+    Serial.println("discharging");
+    state = DISCHARGING;
+  }
+
+
+
+  if (prevState != state) {
+    if (state == CHARGING) {
+      digitalWrite(drainEnableLV, HIGH);
+      delay(100);
+      digitalWrite(chargeEnableLV, LOW);
+
+      digitalWrite(drainIndicator, LOW);
+      digitalWrite(chargeIndicator, HIGH);
+      delay(100);
+      //rechargeCount++;
+    } else if (state == DISCHARGING) {
+      digitalWrite(chargeEnableLV, HIGH);
+      delay(100);
+      digitalWrite(drainEnableLV, LOW);
+
+      digitalWrite(drainIndicator, HIGH);
+      digitalWrite(chargeIndicator, LOW);
+      delay(100);
+    }
+  }
+}
